c116264cdf2b089591432196e3536805e5a12925
etrguez <160863634+etrguez@users.noreply.github.com>
2024-12-08 20:43:05 +0100

    Update animaux.php

--- pages/animaux.php
+++ pages/animaux.php
@@ -64,58 +64,7 @@ require_once '../configuration/config.php';
 
 <!-- 
     START SCRIPT AFFICHAGE ANIMAUX -->
-     <script>
-    function search() {
-    var input = document.getElementById('searchInput').value.toLowerCase();
-    var results = document.getElementById('searchResults');
-    results.innerHTML = '';
-
-    fetch('recherche_animaux.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'query=' + encodeURIComponent(input)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.length > 0) {
-            data.forEach(function(item) {
-                var div = document.createElement('div');
-                div.classList.add('col-md-3');
-                div.style.maxWidth = '200px';
-                div.style.marginBottom = '20px';
-                div.innerHTML = `
-                    <div class="card mb-5" style="border: 1px solid #ddd; border-xradius: 5p;">
-                        <img src="data:image/jpeg;base64,${item.image_data}" class="card-img-top" alt="${item.prenom}" style="border: 2px solid #28a745; width: 100%; height: 100px;">
-                        <div class="card-body" style="padding: 15px;">
-                            <h5 class="card-title" style="font-size: 1.25rem; font-weight: bold;">${item.prenom}</h5>
-                            <p class="card-race" style="font-size: 1rem; color: #6c757d;">Race : ${item.label}</p>
-                            <a href="detail_animal.php?animal_id=${item.animal_id}" class="btn btn-success" style="background-color: #28a745; color: white;">Voir la fiche</a>
-                        </div>
-                    </div>
-                `;
-                results.appendChild(div);
-            });
-        } else {
-            results.innerHTML = 'Aucun résultat trouvé';
-        }
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-        results.innerHTML = 'Une erreur est survenue lors de la recherche.';
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    search();
-});
-</script>
+     <script src="../script/recherche.animaux.js"></script>
 
 <!-- START FOOTER -->
 
