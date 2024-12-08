baa8103abe9ba160ab5619612428230b8c2a3cd5
etrguez <160863634+etrguez@users.noreply.github.com>
2024-12-08 20:27:12 +0100

    Update config.php

--- configuration/config.php
+++ configuration/config.php
@@ -6,18 +6,17 @@ if (getenv('JAWSDB_URL') !== false) {
     $username = $dbparts['user'];
     $password = $dbparts['pass'];
     $database = ltrim($dbparts['path'], '/');
+    $port = isset($dbparts['port']) ? $dbparts['port'] : 3306; 
 } else {
-
     $hostname = 'localhost';
     $username = 'root';
     $password = '';
     $database = 'arcadia';
-    
+    $port = 3306; 
 }
 
 try {
-    
-    $bdd = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+    $bdd = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8mb4", $username, $password);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "";
 } catch (PDOException $e) {
