bba74dfa5e511e26f07adeb4574f5485f26f187f
etrguez <elisabeth.talavera@icloud.com>
2025-01-15 06:30:31 +0100

    modification page creation_animal.php

--- pages/creation_animal.php
+++ pages/creation_animal.php
@@ -105,7 +105,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 ]);
 
                 if ($image_data) {
-                    $stmt = $bdd->prepare('INSERT INTO images (animal_id, image_data) VALUES (:animal_id, :image_data)');
+                    $stmt = $bdd->prepare('UPDATE images SET image_data = :image_data WHERE animal_id = :animal_id');
                     $stmt->execute([
                         ':image_data' => $image_data,
                         ':animal_id' => $animal_id
